4cb25df56789e4d2610c223b32162d84c322ef9e
Shubha <rupelpadhy@gmail.com>
2021-09-14 21:28:45 +0530

    web made

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-alert("working")
+window.onload = function() {
+    document.getElementById("id_capacity_constraint").style.display = "none";
+    document.getElementById("display_contents").style.display = "none";
+  };
+function showCapacityConstraint(){
+    document.getElementById("id_capacity_constraint").style.display = "block";
+}
+
+function onDisplayClick(){
+    document.getElementById("display_contents").style.display = "block";
+}
+// display_contents
