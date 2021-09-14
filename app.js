f87eb5d284700df667bdc1762e5043815a89a226
Shubha <rupelpadhy@gmail.com>
2021-09-14 21:58:18 +0530

    base done

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 window.onload = function() {
+    document.getElementById("id_mdm").style.display = "none";
+    document.getElementById("id_capacity").style.display = "none";
     document.getElementById("id_capacity_constraint").style.display = "none";
     document.getElementById("display_contents").style.display = "none";
   };
@@ -8,5 +10,13 @@ function showCapacityConstraint(){
 
 function onDisplayClick(){
     document.getElementById("display_contents").style.display = "block";
+}
+
+function showCapMat(){
+    document.getElementById("id_capacity").style.display = "block";
+}
+
+function showMDM(){
+    document.getElementById("id_mdm").style.display = "block";
 }
 // display_contents
